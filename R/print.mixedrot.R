7213da743a5232d5b975d77d911bc9c9e070f668
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2014-04-04 16:02:27 +0200

    Added mixedroast function for multiple tests for fixed effects in multiple marginal mixed models

---
+++ R/print.mixedrot.R
@@ -0,0 +1,10 @@
+print.mixedrot <-
+  function(x, digits= max(3, getOption("digits") - 3), ...){  
+    cat("\t mcroast: Multiple Contrast Rotation Tests\n\n")
+    rnames <- paste(rep(rownames(x$B), each=ncol(x$B)), rep(colnames(x$B), times=nrow(x$B)), sep=" @ ")
+    dd <- data.frame(Estimates=as.vector(t(x$B)),
+                     Statistic=as.vector(t(x$statistic)),
+                     "p-values"=as.vector(t(x$pvalue)),
+                     row.names=rnames)
+    printCoefmat(dd, digits=digits, cs.ind=1, tst.int=2, has.Pvalue=TRUE)
+  }
