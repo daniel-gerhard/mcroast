a4042a990e8d8de49e57a9f2f2e94af45769ce8c
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2014-05-19 10:54:54 +0200

    deparse complete lmer formula in mixedroast

--- R/mixedroast.R
+++ R/mixedroast.R
@@ -1,7 +1,7 @@
 mixedroast <- function(formula, data=NULL, K, nrot = 9999, adjusted = TRUE){
   ymat <- model.response(model.frame(as.formula(paste(deparse(formula[[2]], width.cutoff=500), "~ 1")), data=data))
   ynam <- colnames(ymat)
-  forms <- sapply(ynam, function(yna) as.formula(paste(yna, "~", deparse(formula[[3]]))))
+  forms <- sapply(ynam, function(yna) as.formula(paste(yna, "~", paste(deparse(formula[[3]]), collapse=""))))
   lmerlist <- lapply(forms, function(ff) lmer(ff, data=data))
   mixedroastlmerlist(lmerlist=lmerlist, K=K, nrot=nrot, adjusted=adjusted)
 }
